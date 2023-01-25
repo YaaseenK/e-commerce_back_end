9ddd871e303d2ff6049d9cc5af85f77002987e71
YaaseenK <yaaseenkhan02@gmail.com>
2023-01-25 15:02:31 -0500

    first category get route is opperational and includes associated products

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,11 +1,24 @@
 const router = require('express').Router();
+const sequelize = require('../../config/connection');
 const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
-  // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findAll({
+      // be sure to include its associated Products
+      include: { 
+        model: Product,
+        attributes: [`id`, `product_name`, `price`, `stock`, `category_id`],
+      },
+    });
+    return res.status(200).json(categoryData);
+  }
+  catch(err){
+    res.status(404).json(err)
+  }
 });
 
 router.get('/:id', (req, res) => {
@@ -13,8 +26,16 @@ router.get('/:id', (req, res) => {
   // be sure to include its associated Products
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try{
+    const categoryData = await Category.create(req.body);
+    return res.status(200).json(categoryData);
+  }catch (err){
+    res.status(400).json(err);
+  }
+  
+
 });
 
 router.put('/:id', (req, res) => {
